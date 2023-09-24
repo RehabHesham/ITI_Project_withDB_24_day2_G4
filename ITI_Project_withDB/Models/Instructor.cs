0c372e079ffdceceec221b021e990b1a1ab4ee71
Rehab Hesham <rehabhesham98@yahoo.com>
2023-09-24 13:59:57 +0300

    Add project files.

---
+++ ITI_Project_withDB/Models/Instructor.cs
@@ -0,0 +1,16 @@
+﻿namespace ITI_Project_withDB.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Salary { get; set; }
+        public int? Age { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+
+        // Navigation Property
+        public List<Course>? Courses { get; set; }
+    }
+}
